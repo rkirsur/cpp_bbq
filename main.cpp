c4f9462bec6735767cbebc9f88e60a99983de55e
Ritu Kirsur <ritukirsur@gmail.com>
2025-06-30 22:30:44 -0700

    bbq.h

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <chrono>
 #include <stdlib.h>
+#include <unistd.h>
 
 static constexpr uint64_t ITERS = 16;
 static constexpr uint64_t CAPACITY = 16;
@@ -15,8 +16,14 @@ PEX::BBQ::SPSC::Queue<uint64_t, CAPACITY, NUM_OF_BLOCKS> q;
 
 void *writer(void *arg)
 {
-    for (uint64_t i = 0; i < 2; i++) {
+    for (uint64_t i = 0; i < 3; i++) {
+        // if (*(uint64_t *)arg == 1) {
+        //     usleep(10);
+        // }
         while(!q.enqueue(*(uint64_t *)arg)); // enqueue data i
+        // if (*(uint64_t *)arg == 1) {
+        //     usleep(1);
+        // }
         // q.enqueue(*(uint64_t *)arg);
     }
     // (void)arg;
@@ -26,7 +33,7 @@ void *writer(void *arg)
 void *reader(void *arg)
 {
     uint64_t buf;
-    for (uint64_t i = 0; i < ITERS; i++) {
+    for (uint64_t i = 0; i < 2; i++) {
         while(!q.dequeue(buf)); // get data i
         if (buf != i) abort();
     }
@@ -38,17 +45,25 @@ int main(void)
 {
     auto begin = std::chrono::steady_clock::now();
 
+    const uint64_t numThreads = 2;
+    pthread_t t_writerid[numThreads];
+
     uint64_t arg1 = 1;
     uint64_t arg2 = 2;
-    pthread_t t_writer1;
-    pthread_t t_writer2;
+    // pthread_t t_writer1;
+    // pthread_t t_writer2;
+
+    // for (uint64_t i = 0; i < numThreads; i++) {
+    //     uint64_t arg = i + 1;
+    //     pthread_create(&t_writerid[i], NULL, writer, &arg);
+    // }
     // pthread_t t_reader;
-    pthread_create(&t_writer1, NULL, writer, &arg1);
-    pthread_create(&t_writer2, NULL, writer, &arg2);
+    pthread_create(&t_writerid[0], NULL, writer, &arg1);
+    pthread_create(&t_writerid[1], NULL, writer, &arg2);
     // pthread_create(&t_reader, NULL, reader, NULL);
     // pthread_join(t_reader, NULL);
-    pthread_join(t_writer1, NULL);
-    pthread_join(t_writer2, NULL);
+    pthread_join(t_writerid[0], NULL);
+    pthread_join(t_writerid[1], NULL);
 
     q.printData();
 
